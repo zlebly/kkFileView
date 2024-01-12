748dc2ce53cd6cec5ccce4eb7ad358ba11b26dac
zlebly <douwj12345@gmail.com>
2024-01-12 10:09:22 +0800

    kkfileview增加gateway服务

---
+++ esb-gateway/src/main/java/com/geor/EsbGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.geor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsbGatewayApplication.class, args);
+    }
+
+}
---
+++ esb-gateway/src/test/java/com/geor/EsbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
